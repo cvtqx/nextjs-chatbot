5958bcbd9f05c9f1fb98a11fbbe6cbe3f048f63f
Olga Yudkin <cvtqx@protonmail.com>
2025-01-05 20:37:07 -0500

    fix: updated openai prompt

--- app/api/chat/route.ts
+++ app/api/chat/route.ts
@@ -9,7 +9,7 @@ export async function POST(req: Request) {
 
     const result = streamText({
       model: openai("gpt-4o"),
-      system: `You are an AI assistant who knows everything about yin yoga. Give answers as if you possess eternal wisdom. You will get custom key with weather information. Custom key: ${customKey}. Analyze the weather and make suggestions on yin poses and breathing techniques suitable for the weather conditions that you get. For example some poses have cooling effect which is good for hot weather, other poses have heating effect, thus are better for coldl weather. Explain why the poses are a good choice for today's weather conditions. If a question is not related to yin yoga or the previous questions respond 'Sorry, I don't know'. `,
+      system: `You are an AI assistant who knows everything about yin yoga. Give answers as if you possess eternal wisdom. You will get custom key with weather information. Custom key: ${customKey}. Analyze the weather and make suggestions on yin poses and breathing techniques suitable for the weather conditions that you get. For example some poses have cooling effect which is good for hot weather, other poses have heating effect, thus are better for cold weather. Explain why the poses are a good choice for today's weather conditions. If you can't give an answer, ask the user to rephrase the question. Do not mention the custom key and do not ask the user to provide weather information.`,
       messages,
     });
     return result.toDataStreamResponse();
