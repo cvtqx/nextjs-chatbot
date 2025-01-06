7ba2fe3162092e270a2d56a0993acd1fe0671dd3
Olga Yudkin <cvtqx@protonmail.com>
2025-01-05 21:40:00 -0500

    chore: deleted unused file

--- scripts/loadDb.ts
+++
@@ -1,5 +0,0 @@
-import { DataAPIClient } from '@datastax/astra-db-ts'
-import { PuppeteerWebBaseLoader } from '@langchain/community/document_loaders/web/puppeteer'
-import OpenAI from 'openai'
-import { RecursiveCharacterTextSplitter } from "@langchain/textsplitters"
-import 'dotenv/config'
