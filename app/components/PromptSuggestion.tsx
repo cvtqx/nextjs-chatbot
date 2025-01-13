06d1365f1aa3f18b4057d1742700fc7d7702694a
Olga Yudkin <cvtqx@protonmail.com>
2025-01-13 14:23:16 -0500

    fix: changes div to buttons in promptsuggestion

--- app/components/PromptSuggestion.tsx
+++ app/components/PromptSuggestion.tsx
@@ -2,11 +2,11 @@ import React from 'react'
 
 const PromptSuggestion = ({ prompt, onClick }) => {
   return (
-    <div className="card bg-gray-100 w-48 flex-shrink-0 hover:bg-gray-200 hover:shadow-lg transition-all duration-300" onClick={onClick}>
+    <button className="card bg-gray-100 w-48 flex-shrink-0 hover:bg-gray-200 hover:shadow-lg transition-all duration-300" onClick={onClick}>
       <div className="card-body">
         <p>{prompt}</p>
       </div>
-    </div>
+    </button>
     
   )
 }
